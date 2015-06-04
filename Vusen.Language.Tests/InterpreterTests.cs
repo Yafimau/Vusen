0c260e6153194bf2d27199ef498b02317258fba8
dyafimau <efimov.deniz@gmail.com>
2015-06-04 13:52:17 +0300

    First classes and tests added

---
+++ Vusen.Language.Tests/InterpreterTests.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InterpreterTests.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The interpreter tests.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Vusen.Language.Tests
+{
+    using NUnit.Framework;
+
+    using Vusen.Language.Entities;
+
+    /// <summary>
+    /// The interpreter tests.
+    /// </summary>
+    [TestFixture]
+    public class InterpreterTests
+    {
+        private Interpreter interpreter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.interpreter = new Interpreter();
+        }
+
+        /// <summary>
+        /// The run test.
+        /// </summary>
+        [Test]
+        public void RunTest()
+        {
+            var script = new Script();
+
+            var result = this.interpreter.Run(script);
+
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ Vusen.Language/Interpreter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vusen.Language
+{
+    using Vusen.Language.Entities;
+
+    public class Interpreter
+    {
+        public RunResult Run(Script script)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
